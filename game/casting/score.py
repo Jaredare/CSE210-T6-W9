cd1889a58cdb9f1270c0efd3f621892fdbeeb3f0
kaeellyn <pap21006@byui.edu>
2022-03-09 12:38:00 +0800

    scores update
    Co-authored-by: IAmLiamH <IAmLiamH@users.noreply.github.com>

--- game/casting/score.py
+++ game/casting/score.py
@@ -1,4 +1,7 @@
 from game.casting.actor import Actor
+from game.shared import constants as constants
+from game.shared.point import Point
+from game.casting.cast import Cast
 
 
 class Score(Actor):
@@ -12,16 +15,48 @@ class Score(Actor):
     Attributes:
         _points (int): The points earned in the game.
     """
-    def __init__(self):
+    def __init__(self, player):
         super().__init__()
         self._points = 0
-        self.add_points(0)
+        self.add_points(0, player)
+        
 
-    def add_points(self, points):
+    def add_points(self, points, player):
         """Adds the given points to the score's total points.
         
         Args:
             points (int): The points to add.
         """
         self._points += points
-        self.set_text(f"Score: {self._points}")
+        self.set_text(f"Player {player}: {self._points}")
+
+    def prepare_score(self, score):
+        """Prepares the body of the score"""
+        cast = Cast()
+        # scores = cast.get_actors("scores")
+
+        # score1 = scores[0]
+        # score2 = scores[1]
+
+        print("Preparing score")
+
+        if score == "One":
+            """This is for the first player's score. """
+            score = Score("One")
+            x = int(constants.MAX_X / 4 * 1)
+            y = int(constants.MAX_Y)
+        
+        elif score == "Two":
+            """This is for the second player's score. """
+            score = Score("Two")
+            x = int(constants.MAX_X / 4 * 3)
+            y = int(constants.MAX_Y / 4)
+        
+        else:
+            x = int(constants.MAX_X / 2)
+            y = int(constants.MAX_Y / 2)
+        
+        position = Point(x, y)
+        Score.set_position(position)
+            
+        
